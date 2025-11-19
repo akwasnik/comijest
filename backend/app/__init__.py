c0c0aae59c8658b851179276ff51bd807c02f66b
Osodsa <minkaler@gmail.com>
2025-11-19 22:56:12 +0100

    added connection do mongodb cluster for testing

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from pymongo import PyMongo
+
+mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config')
-    # Register blueprints here
+    
+    mongo.init_app(app)
+
+    # Blueprints (routes from express) here
 
     return app
-    
+
+
+def get_db():
+    return mongo.db
