42448ba69c7251a66f8a4253d305dc2530726795
akwasnik <adrian.kwasnik@gmail.com>
2025-11-22 18:56:30 +0100

    feat(theme-context): added theme to the page

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import { User } from "lucide-react";
+import { AnimatedThemeToggler } from "./ui/animated-theme-toggler";
 
 export default function Header() {
   return (
-    <header className="w-full bg-white shadow-sm shadow-red-300 py-5 px-6">
+    <header className="w-full bg-transparent shadow-sm shadow-red-300 py-5 px-6">
       <div className="mx-auto flex justify-between items-center">
         
         <a href="/" className="flex flex-col items-start">
@@ -23,7 +24,7 @@ export default function Header() {
             className="absolute top-11 left-5.5"
           />
         </a>
-
+        <AnimatedThemeToggler />
         <div className="flex items-center gap-6 pr-2">
           <a href="/about" className="text-md font-bold text-red-400 hover:text-red-700">
             O nas
