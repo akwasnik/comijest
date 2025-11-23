d7c968846d6e4031c32310e65392e9d3397169b7
akwasnik <adrian.kwasnik@gmail.com>
2025-11-23 12:46:05 +0100

    fix(theme): optimized and not stored in localestorage also deleted context and used next-themes for theme

--- frontend/app/components/ui/animated-theme-toggler.tsx
+++ frontend/app/components/ui/animated-theme-toggler.tsx
@@ -1,16 +1,15 @@
-"use client"
+"use client";
 
-import { useCallback, useEffect, useRef } from "react"
-import { Moon, Sun } from "lucide-react"
-import { flushSync } from "react-dom"
-
-import { cn } from "@/app/lib/utils"
-import { useThemeContext  } from "@/app/context/theme-context"
-
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Moon, Sun } from "lucide-react";
+import { flushSync } from "react-dom";
+import { cn } from "@/app/lib/utils";
+import { Spinner } from "./spinner";
+import { useTheme } from "next-themes";
 
 interface AnimatedThemeTogglerProps
   extends React.ComponentPropsWithoutRef<"button"> {
-  duration?: number
+  duration?: number;
 }
 
 export const AnimatedThemeToggler = ({
@@ -18,53 +17,34 @@ export const AnimatedThemeToggler = ({
   duration = 400,
   ...props
 }: AnimatedThemeTogglerProps) => {
+  const [mounted, setMounted] = useState(false)
+  const {setTheme, resolvedTheme} =  useTheme();
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
-  const {isDark, setIsDark} = useThemeContext();
-  
-  const buttonRef = useRef<HTMLButtonElement>(null)
-
-  useEffect(() => {
-    const updateTheme = () => {
-      setIsDark(document.documentElement.classList.contains("dark"))
-    }
-
-    updateTheme()
-
-    const observer = new MutationObserver(updateTheme)
-    observer.observe(document.documentElement, {
-      attributes: true,
-      attributeFilter: ["class"],
-    })
-
-    return () => observer.disconnect()
-  }, [setIsDark])
-
-  const toggleTheme = useCallback(async () => {
-    if (!buttonRef.current) return
+    const toggleTheme = useCallback(async () => {
+    if (!buttonRef.current) return;
+    const newTheme = resolvedTheme === "dark" ? false : true;
 
     await document.startViewTransition(() => {
       flushSync(() => {
-        const newTheme = !isDark
-        setIsDark(newTheme)
-        document.documentElement.classList.toggle("dark")
-        localStorage.setItem("theme", newTheme ? "dark" : "light")
-      })
-    }).ready
-
+        setTheme(newTheme ? "dark" : "light");
+      });
+    }).ready;
+    
     const { top, left, width, height } =
-      buttonRef.current.getBoundingClientRect()
-    const x = left + width / 2
-    const y = top + height / 2
+    buttonRef.current.getBoundingClientRect();
+    const x = left + width / 2;
+    const y = top + height / 2;
     const maxRadius = Math.hypot(
       Math.max(left, window.innerWidth - left),
       Math.max(top, window.innerHeight - top)
-    )
+    );
 
     document.documentElement.animate(
       {
         clipPath: [
           `circle(0px at ${x}px ${y}px)`,
-          `circle(${maxRadius}px at ${x}px ${y}px)`,
+          `circle(${maxRadius}px at ${x}px ${y}px)`
         ],
       },
       {
@@ -72,18 +52,28 @@ export const AnimatedThemeToggler = ({
         easing: "ease-in-out",
         pseudoElement: "::view-transition-new(root)",
       }
-    )
-  }, [isDark, duration, setIsDark])
+    );
+    
+  }, [duration, resolvedTheme, setTheme]);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return (
+      <Spinner className="text-red-500"/>
+  );
 
   return (
     <button
       ref={buttonRef}
       onClick={toggleTheme}
-      className={cn(className,"text-red-500")}
+      className={cn(className, "text-red-500")}
       {...props}
     >
-      {isDark ? <Sun /> : <Moon />}
+      {resolvedTheme === "dark" ? <Sun /> : <Moon />}
       <span className="sr-only">Toggle theme</span>
     </button>
-  )
-}
+  );
+};
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,33 +2,23 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
-
-// LENIS SMOOTH SCROLL
-
 import SmoothScrollProvider from "./providers/SmoothScrollProvider";
-import ThemeContextProvider from "./context/theme-context";
-
+import CustomThemeProvider from "./providers/ThemeProvider";
 
 export const metadata: Metadata = {
   title: "Comijest",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pl">
-      
-        <body className="min-h-screen flex flex-col">
-          <ThemeContextProvider>
-            <Header />
-            <SmoothScrollProvider>{children}</SmoothScrollProvider>
-            <Footer />
-          </ThemeContextProvider>
-        </body>
+    <html lang="pl" suppressHydrationWarning>
+      <body className="min-h-screen flex flex-col">
+        <CustomThemeProvider>
+          <Header />
+          <SmoothScrollProvider>{children}</SmoothScrollProvider>
+          <Footer />
+        </CustomThemeProvider>
+      </body>
     </html>
   );
 }
