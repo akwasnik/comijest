97404f05faea616ae7eab46b81c2113f480c47ee
akwasnik <adrian.kwasnik@gmail.com>
2025-11-27 09:52:01 +0100

    feat(Components/GlobalLoader): implemented globalLoader component

---
+++ frontend/app/components/GlobalLoader.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+export default function GlobalLoader() {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const MIN_TIME = 600;
+
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, MIN_TIME);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div
+      className={`
+        fixed inset-0 z-[9999]
+        flex items-center justify-center
+        bg-white dark:bg-neutral-950
+        transition-opacity duration-500 
+        ${visible ? "opacity-100" : "opacity-0 pointer-events-none"}
+      `}
+    >
+      
+      <div className="flex flex-col items-center gap-3">
+        <Image
+          src="/logo.png"
+          width={70}
+          height={70}
+          alt="Comijest logo"
+          className="animate-pulse"
+        />
+        <p className="text-red-500 font-semibold animate-pulse">
+          Ładowanie...
+        </p>
+      </div>
+    </div>
+  );
+}
