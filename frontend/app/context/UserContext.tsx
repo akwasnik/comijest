0ef6f0961ebdbc7c6ccd83ade9aa82c5483f4929
akwasnik <adrian.kwasnik@gmail.com>
2026-01-25 21:19:59 +0100

    frontend edit for prod

--- frontend/app/context/UserContext.tsx
+++ frontend/app/context/UserContext.tsx
@@ -34,7 +34,7 @@ export function UserProvider({ children }: { children: ReactNode }) {
     queryKey: ["currentUser"],
     queryFn: async () => {
       const res = await fetch(
-        "http://localhost:5000/api/users/me",
+        "http://backend:5000/api/users/me",
         {
           credentials: "include",
         }
@@ -56,7 +56,7 @@ export function UserProvider({ children }: { children: ReactNode }) {
   const logoutMutation = useMutation({
     mutationFn: async () => {
       const res = await fetch(
-        "http://localhost:5000/api/users/logout",
+        "http://backend:5000/api/users/logout",
         {
           method: "POST",
           credentials: "include",
